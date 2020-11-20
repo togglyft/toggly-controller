e862209de8bc036ac99c29525c9fc87633274550
Pablo Ubal <pablo.ubal@gmail.com>
2020-11-21 00:01:57 +0100

    Fix/push to docker org

--- dockerPush.sh
+++ dockerPush.sh
@@ -2,5 +2,6 @@
 PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{print $2}' | sed 's/[",]//g' | tr -d '[[:space:]]')
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{print $2}' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker login --username=pabloubal
-docker tag $PACKAGE_NAME:$PACKAGE_VERSION pabloubal/$PACKAGE_NAME:$PACKAGE_VERSION
-docker push pabloubal/$PACKAGE_NAME:$PACKAGE_VERSION
+docker tag $PACKAGE_NAME:$PACKAGE_VERSION togglyft/$PACKAGE_NAME:$PACKAGE_VERSION
+docker tag togglyft/$PACKAGE_NAME:$PACKAGE_VERSION togglyft/$PACKAGE_NAME:latest
+docker push togglyft/$PACKAGE_NAME:$PACKAGE_VERSION
