a2cde2b98fce7d53123cb8ae4c3c587808d7b7dd
marcos.pacheco <marcos.pacheco@flexit.es>
2021-09-29 15:47:49 +0200

    added company id

--- src/entities/feature.entity.ts
+++ src/entities/feature.entity.ts
@@ -1,9 +1,10 @@
-function Feature(id, name, enabled, relatedTask, lastModified) {
+function Feature(id, name, enabled, relatedTask, lastModified, company_id) {
   this.id = id;
   this.name = name;
   this.enabled = enabled;
   this.relatedTask = relatedTask;
   this.lastModified = lastModified;  
+  this.company_id = company_id;  
 }
 
 module.exports = Feature
